5a6b5e57107a01041c88b93f5f4969e6777b31f7
Benjamin <benjaminayimah@gmail.com>
2025-12-29 23:20:29 +0000

    commit

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -11,29 +11,16 @@ import PageTransition from '../components/PageTransition';
 export default function RootLayout({ children }) {
   const { device } = useUIStore();
 
-  console.log("Current device:", device);
-
   return (
     <ReactLenis
       root
-      autoRaf={device !== 'mobile'} // keeps Lenis synced with requestAnimationFrame 
+      autoRaf={device !== 'mobile'}
       options={{
         duration: 1.2,
         smoothWheel: device !== 'mobile',
         smoothTouch: false,
         lerp: 0.08,
       }}
-      // root
-      // autoRaf={true} // keeps Lenis synced with requestAnimationFrame
-      // options={{
-      //   // 👇 Tweaked values for smoother, less jumpy experience
-      //   duration: 1.2,      // Easing duration (higher = smoother, lower = snappier)
-      //   smoothWheel: true,
-      //   smoothTouch: false,
-      //   syncTouch: true,    // Helps when scrolling with touch + wheel mix
-      //   touchMultiplier: 1.2,
-      //   lerp: 0.08,         // Leave this if you like your current easing feel
-      // }}
     >
       <main id="app">
         <PageTransition>

a20b65657ea1b66af93e1872efae233736c4ee87
Benjamin <benjaminayimah@gmail.com>
2026-01-10 12:35:51 +0000

    commit

--- app/components/dashboard/VerifySuccess.jsx
+++ app/components/dashboard/VerifySuccess.jsx
@@ -42,7 +42,7 @@ function VerifySuccess({ invoice }) {
               This invoice is duly verified as an authentic copy and can be safely used for record-keeping and payment purposes. If you have any questions or concerns, please contact our support team.
             </p>
             <a href='/' className="myHover-translate mt-6 w-full py-2.5 bg-black text-white rounded-3xl hover:bg-gray-900 font-medium inline-block">
-              Contact support
+              Back to Home
             </a>
           </div>
           <VerifyFooter />

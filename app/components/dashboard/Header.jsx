8fc4facec83a3dd449385177707d7245cc49c841
Benjamin <benjaminayimah@gmail.com>
2026-08-15 13:38:56 +0000

    company info

--- app/components/dashboard/Header.jsx
+++ app/components/dashboard/Header.jsx
@@ -88,7 +88,7 @@ function Header() {
               <Cog6ToothIcon strokeWidth={2} className="h-4.5" />
               Settings
             </Link>
-            <div className="border-b border-b-gray-100 my-1 mx-2" />
+            <div className="border-b border-b-gray-200/80 my-1.5 mx-2" />
             <a
               target='_blank'
               href='https://sso.secureserver.net/login?app=email&realm=pass'
@@ -100,7 +100,6 @@ function Header() {
               </span>
               <ArrowTopRightOnSquareIcon className="h-4" />
             </a>
-            {/* <div className="border-b border-b-gray-100 my-0.5 mx-2" /> */}
             <button
               className="flex items-center gap-1.5 w-full text-left px-3 py-2 text-red-600 hover:bg-red-50 text-sm font-medium transition"
               onClick={handleLogout}
